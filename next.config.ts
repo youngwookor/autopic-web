7cf9b8ef72dbb33b7784acce58b067596644eef2
youngwookor <your@email.com>
2025-12-23 18:25:16 +0900

    fix: add rewrites to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: 'http://43.200.229.169:8000/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
